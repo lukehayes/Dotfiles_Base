f18a9dc58f285843af79cee6a583fb4f59e0545e
Luke Hayes <lukepurplehayes@gmail.com>
2021-11-22 10:16:30 +0000

    Added Vim plugin cloning, fonts, wallpapers and more to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/sh
 
+# ----------------------------------------
+# Variables
+# ----------------------------------------
+VIM_RUNTIME_DIR="$HOME/.vim_runtime"
+VIM_PLUGIN_DIR="$VIM_RUNTIME_DIR/my_plugins"
+VIM_PLUGINS=(
+    "git@github.com:flazz/vim-colorschemes.git"
+    "git@github.com:preservim/nerdcommenter.git"
+    "git@github.com:vimwiki/vimwiki.git"
+    )
+FONTS_DIR="JetBrainsMono"
+PROJECT_DIR="$HOME/Desktop/Linux-Config"
+
+
+# ----------------------------------------
+# Move Configuration Files
+# ----------------------------------------
+echo "Copying all configuration files..."
 function copyFile()
 {
     echo "Copying $1 to location $2"
@@ -8,13 +26,39 @@ function copyFile()
     echo -e "\n"
 }
 
-copyFile bashrc-aliases $HOME/.config/luke-config/
+# Bash
+copyFile "bashrc-aliases" $HOME/.config/luke-config/
 
+# BSPWM
 copyFile "bspwm/keybindings" $HOME/.config/bspwm
 
+# Polybar
 copyFile "polybar/config.ini" $HOME/.config/polybar
 
+# SXHKD
 copyFile "sxhkd/sxhkdrc" $HOME/.config/sxhkd
+
+# ----------------------------------------
+# Clone Vim Plugins
+# ----------------------------------------
+echo "Cloning VIM plugins to $VIM_PLUGIN_DIR"
+
+cd $VIM_PLUGIN_DIR
+for i in "${VIM_PLUGINS[@]}"; do
+    git clone $i
+done
+
+echo "Cloning VIM plugins to $VIM_PLUGIN_DIR finished."
+
+cd $PROJECT_DIR
+
+# ----------------------------------------
+# Move Fonts Folder
+# ----------------------------------------
+
+echo "Copying $FONTS_DIR to $HOME/.fonts"
+cp -r $FONTS_DIR $HOME/.fonts
+echo "Finised $FONTS_DIR to $HOME/.fonts"
 
 echo "☻ Script installation finished."
 echo -e "\n"
